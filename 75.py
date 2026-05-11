9965a15618ba23dc19396494dae3308957d6a4ff
damian.gao <593700742@qq.com>
2026-05-11 09:41:14 +0800

    Add color sorting method using Dutch National Flag
    Implemented Dutch National Flag algorithm to sort colors in-place.

--- 75.py
+++ 75.py
@@ -1,9 +1,25 @@
 class Solution:
+    def swap(self, a, b, nums):
+        nums[a], nums[b] = nums[b], nums[a]
+
     def sortColors(self, nums: List[int]) -> None:
         """
-        75. Sort Colors (Dutch National Flag)
-        Medium
-
-        三向切分：0=红色, 1=白色, 2=蓝色（原地修改）
+        Do not return anything, modify nums in-place instead.
         """
-        pass
+        l = curr = 0 # 左指针保证 l - 1以及之前的都是0
+        r = len(nums)-1 # 右指针保证 r + 1以及之后的都是1
+        while curr <= r:
+            if nums[curr] == 0 :
+                self.swap(l, curr, nums)
+                l += 1
+                curr += 1
+            elif nums[curr] == 2:
+                self.swap(r, curr, nums)
+                r -= 1
+            else:
+                curr += 1
+            
+
+
+
+
