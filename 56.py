74c77f79e1c52545a32f80978ec7b802371a98ed
Damian <damian@example.com>
2026-08-26 21:02:13 +0800

    Fix 56. mergeIntervals: sort result kept, max end, final append

--- 56.py
+++ 56.py
@@ -1,4 +1,9 @@
 class Solution:
+    def merge_single(self,a,b):
+        return [a[0], max(a[1],b[1])]
+    def can_merge(self,a,b):
+        return a[1]>=b[0]
+
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         """
         56. Merge Intervals
@@ -6,4 +11,20 @@ class Solution:
 
         合并重叠的区间
         """
-        pass
+        ret = []
+        intervals=sorted(intervals, key=lambda x:x[0])
+
+        if len(intervals) <=1:
+            return intervals
+        curr = intervals[0]
+        i = 1
+        while i < len(intervals):
+            if self.can_merge(curr, intervals[i]):
+                curr =self.merge_single(curr, intervals[i])
+            else:
+                ret.append(curr)
+                curr = intervals[i]
+            i += 1
+
+        ret.append(curr)
+        return ret
